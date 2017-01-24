1e2d9af4d2448272c5cfb2e42ac5e7cb9d5ddaab
Jerry <Jeremias.Boos@gmx.de>
2017-01-24 10:14:48 +0100

    added assetes and scripts

---
+++ Assets/Scripts/Level_scripts/generate_piste.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generate_piste : MonoBehaviour {
+
+	public GameObject[] obstacles;
+	public Vector2 grid_size = new Vector2(1,1);
+
+	[Range(0,1)]
+	public float placement_rate;
+
+	// Use this for initialization
+	void Start () {
+		var collider = GetComponent<Collider> ();
+
+		for (float x = collider.bounds.min.x; x < collider.bounds.max.x; x += grid_size.x)
+			for (float z = collider.bounds.min.z; z < collider.bounds.max.z; z += grid_size.y)
+				if (Random.value < placement_rate) {
+					RaycastHit hit_info;
+					if (Physics.Raycast (new Vector3 (x, collider.bounds.max.y, z), Vector3.down, out hit_info)) {
+						var obstacle = obstacles [Random.Range (0, obstacles.Length)];
+						var go = GameObject.Instantiate<GameObject> (obstacle, hit_info.point, obstacle.transform.rotation, transform);
+						go.transform.localScale = Vector3.one*2;
+					}
+				}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Unit_Controlles/Char_Controlle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Char_Controlle : MonoBehaviour {
+
+	private CharacterController character_controller;
+	public Bounds move_bounds;
+
+	// Use this for initialization
+	void Start () {
+		character_controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float y = 0;
+		if (!character_controller.isGrounded) {
+			y = -1;
+		}
+		var move_vec = new Vector3 (Input.GetAxis ("Horizontal"), y, Input.GetAxis ("Vertical"));
+		if (!move_bounds.Contains (character_controller.transform.position + move_vec)) {
+			move_vec.x = 0;
+			move_vec.z = 0;
+		}
+		character_controller.Move (move_vec);
+
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube (move_bounds.center, move_bounds.size);
+	}
+}
